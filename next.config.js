5dc0e58b2c64ebaf01816cf9e3a8bf4bc923f42b
ASAP ESTATE <admin@asapestate.com>
2025-09-05 22:32:47 +0700

    chore: trigger redeploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // เพิ่มคอนฟิกอื่นๆ ได้ภายหลัง เช่น images, redirects, headers ฯลฯ
-};
-
-module.exports = nextConfig; // ใช้แบบ CommonJS (แนะนำ)
+const nextConfig = {};
+module.exports = nextConfig;
